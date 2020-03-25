b890043d4e7fc8e9ac49a6d3fe2150cc93eb59a4
Matthias Noback <matthiasnoback@gmail.com>
2020-03-25 16:46:53 +0100

    Add a simple EmailAddress value object

---
+++ src/Domain/Model/Member/EmailAddress.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+namespace LeanpubBookClub\Domain\Model\Member;
+
+use Assert\Assert;
+
+final class EmailAddress
+{
+    private string $emailAddress;
+
+    private function __construct(string $emailAddress)
+    {
+        Assert::that($emailAddress)->email();
+
+        $this->emailAddress = $emailAddress;
+    }
+
+    public static function fromString(string $string): self
+    {
+        return new self($string);
+    }
+
+    public function asString(): string
+    {
+        return $this->emailAddress;
+    }
+}
---
+++ test/Unit/LeanpubBookClub/Domain/Model/Member/EmailAddressTest.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace LeanpubBookClub\Domain\Model\Member;
+
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+
+final class EmailAddressTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_requires_the_provided_string_to_look_like_a_valid_email_address(): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        EmailAddress::fromString('does-not-look-good');
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_be_converted_back_to_a_string(): void
+    {
+        self::assertEquals(
+            'info@matthiasnoback.nl',
+            EmailAddress::fromString('info@matthiasnoback.nl')->asString()
+        );
+    }
+}
