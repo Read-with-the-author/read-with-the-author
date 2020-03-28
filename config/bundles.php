cf6d1e8f9845bbb8a9fe58d68a96c3e4217676d1
Matthias Noback <matthiasnoback@gmail.com>
2020-03-28 16:56:23 +0100

    Install logger

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
 ];
