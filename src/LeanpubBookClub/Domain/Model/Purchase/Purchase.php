d3bce064d26ea95cc9cb799bdf870303782213b2
Matthias Noback <matthiasnoback@gmail.com>
2020-04-02 18:01:27 +0200

    Add mapping for Purchase aggregate

--- src/LeanpubBookClub/Domain/Model/Purchase/Purchase.php
+++ src/LeanpubBookClub/Domain/Model/Purchase/Purchase.php
@@ -3,10 +3,12 @@ declare(strict_types=1);
 
 namespace LeanpubBookClub\Domain\Model\Purchase;
 
+use Doctrine\DBAL\Schema\Schema;
 use LeanpubBookClub\Domain\Model\Common\Entity;
 use LeanpubBookClub\Domain\Model\Member\LeanpubInvoiceId;
+use TalisOrm\Schema\SpecifiesSchema;
 
-final class Purchase
+final class Purchase implements SpecifiesSchema
 {
     use Entity;
 
@@ -43,5 +45,15 @@ final class Purchase
     public function leanpubInvoiceId(): LeanpubInvoiceId
     {
         return $this->leanpubInvoiceId;
+    }
+
+    public static function specifySchema(Schema $schema): void
+    {
+        $table = $schema->createTable('purchases');
+
+        $table->addColumn('leanpubInvoiceId', 'string');
+        $table->setPrimaryKey(['leanpubInvoiceId']);
+
+        $table->addColumn('wasClaimed', 'boolean')->setNotnull(true);
     }
 }
